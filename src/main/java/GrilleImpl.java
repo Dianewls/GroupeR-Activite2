0a48dd5c8b4c30019f02fab63ab3a81d3c476969
diane <dianewoulsou@gmail.com>
2022-03-29 20:04:55 +0200

    IMPL

--- src/main/java/GrilleImpl.java
+++ src/main/java/GrilleImpl.java
@@ -1,15 +1,52 @@
 
 public class GrilleImpl implements Grille{
+	/**
+	 * 
+	 */
+	private char[][] grille;
+	/**
+	 * 
+	 */
 	private int longueur;
+	/**
+	 * 
+	 */
 	private int largeur;
-	
 	 /**
+     * @var GRILLE9x9SIZE taille du tableau 9*9.
+     */
+    private static final int GRILLE9x9SIZE = 9;
+    /**
+     * @var SIZEE taille du tableau 16*16.
+     */
+    private static final int GRILLE16x16SIZE = 16;
+    /**
+     * Caractere possible a mettre dans la grille.
+     * pour une grille 9x9 : 1..9
+     * pour une grille 16x16: 0..9-a..f
+     */
+    private static final char[] CHARPOSSIBLE = new char[]
+            {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	public GrilleImpl() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	 public GrilleImpl(char[][] unegrille) {
+		super();
+		if (unegrille.length == GRILLE9x9SIZE || unegrille.length == GRILLE16x16SIZE) {
+			this.grille = unegrille;
+		}else {
+			 throw new IllegalArgumentException("Le tableau doit être de dimension 9x9 ou 16x16");
+		}
+	}
+
+	/**
 	  * @return largeur/hauteur de la grille
 	  */
 	@Override
 	public int getDimension() {
-		// TODO Auto-generated method stub
-		return 0;
+		return grille.length;
 	}
 	 /**
 	  * Affecte une valeur dans la grille
@@ -26,9 +63,32 @@ public class GrilleImpl implements Grille{
 	  */
 	@Override
 	public void setValue(int x, int y, char value) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		
+		if (x < 0 || x > this.getDimension()
+                || y < 0 || y > this.getDimension()) {
+            throw new IllegalArgumentException("x et/ou y est(sont) hors des dimensions");
+        } else {
+            if (!belong(value)) {
+                throw new IllegalArgumentException("Caractere non autorise");
+            }
+        }
+        grille[x][y] = value;
 	}
+	
+	 /**
+     * Verify if the character to add is authorized.
+     * @param c caractere a verifier
+     * @return true or false
+     */
+    public final boolean belong(final char c) {
+        if (grille.length == GRILLE9x9SIZE) {
+            for (char s : CHARPOSSIBLE) {
+                if (s == c) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 	/**
 	  * Recupere une valeur de la grille
 	  * 
