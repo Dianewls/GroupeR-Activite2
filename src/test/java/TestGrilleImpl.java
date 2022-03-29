1f6b56405bfc5b03d3026304ece0253ff7966abb
Tiguiboss <tdiao1999@gmail.com>
2022-03-29 23:58:47 +0200

    commit checkstyle1

--- src/test/java/TestGrilleImpl.java
+++ src/test/java/TestGrilleImpl.java
@@ -1,12 +1,16 @@
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-
+/*
+ * Classe pour les tests.
+ */
 public class TestGrilleImpl {
-
-	   private char[][] Grille9x9Aremplir = {{'@' , '6', '@', '@', '4', '5', '3', '7', '@'},
+	   /*
+	    * grille de taille 9 de base.
+	    */
+	   private char[][] Grille9x9Aremplir = {
+			   {'@', '6', '@', '@', '4', '5', '3', '7', '@'},
 			   {'@', '@', '5', '6', '7', '3', '4', '2', '@'},
 			   {'@', '@', '4', '@', '@', '@', '@', '@', '1'},
 			   {'5', '@', '@', '7', '@', '2', '@', '@', '4'},
@@ -15,8 +19,11 @@ public class TestGrilleImpl {
 			   {'4', '9', '@', '5', '1', '7', '8', '@', '@'},
 			   {'2', '1', '@', '@', '3', '6', '@', '@', '@'},
 			   {'@', '5', '@', '@', '2', '@', '1', '@', '@'}};
-	   
-	   private char[][] Grille9x9Remplie = {{'9' , '6', '2', '1', '4', '5', '3', '7', '8'},
+	   /*
+	    * grille de taille 9 resolue.
+	    */
+	   private char[][] Grille9x9Remplie = {
+			   {'9', '6', '2', '1', '4', '5', '3', '7', '8'},
 			   {'1', '8', '5', '6', '7', '3', '4', '2', '9'},
 			   {'3', '7', '4', '2', '9', '8', '5', '6', '1'},
 			   {'5', '3', '1', '7', '6', '2', '9', '8', '4'},
@@ -25,8 +32,11 @@ public class TestGrilleImpl {
 			   {'4', '9', '6', '5', '1', '7', '8', '3', '2'},
 			   {'2', '1', '8', '9', '3', '6', '7', '4', '5'},
 			   {'7', '5', '3', '8', '2', '4', '1', '9', '6'}};
-	   
-	   private char[][] Grille16x16Aremplir = {{'@' , '2', '@', '@', '8', '@', 'B', '@', '@', '@', '@', '@', '@', '@', '5', '7'},
+	   /*
+	    * grille de taille 16 de base.
+	    */
+	   private char[][] Grille16x16Aremplir = {
+			   {'@', '2', '@', '@', '8', '@', 'B', '@', '@', '@', '@', '@', '@', '@', '5', '7'},
 			   {'9', 'F', 'C', '@', '@', '1', '@', '@', '@', '8', '5', 'B', 'E', '4', 'A', '2'},
 			   {'@', '@', '@', '1', '@', 'A', '4', '2', '@', '@', '@', '7', 'G', 'B', '9', '@'},
 			   {'@', '@', 'A', 'B', 'C', 'D', '5', '@', '@', '@', '2', '4', '3', '8', '6', '@'},
@@ -42,8 +52,11 @@ public class TestGrilleImpl {
 			   {'6', '@', '@', 'C', '4', '@', '7', '1', '@', '@', 'G', '@', 'B', '@', 'F', '@'},
 			   {'@', '1', '@', '4', '3', 'B', '@', '@', '5', '9', '@', 'D', '@', '@', '8', 'G'},
 			   {'G', '@', '3', 'D', 'A', 'F', '@', '@', 'B', '4', '@', 'C', '@', '@', '2', '@'}};
-	   
-	   private char[][] Grille16x16Remplie = {{'E' , '2', '4', '6', '8', '3', 'B', 'F', 'A', '1', '9', 'G', 'D', 'C', '5', '7'},
+	   /*
+	    * grille de taille 16 resolue.
+	    */
+	   private char[][] Grille16x16Remplie = {
+			   {'E', '2', '4', '6', '8', '3', 'B', 'F', 'A', '1', '9', 'G', 'D', 'C', '5', '7'},
 			   {'9', 'F', 'C', '3', 'G', '1', '6', '7', 'D', '8', '5', 'B', 'E', '4', 'A', '2'},
 			   {'D', '8', '5', '1', 'E', 'A', '4', '2', '3', '6', 'C', '7', 'G', 'B', '9', 'F'},
 			   {'7', 'G', 'A', 'B', 'C', 'D', '5', '9', 'E', 'F', '2', '4', '3', '8', '6', '1'},
@@ -59,7 +72,9 @@ public class TestGrilleImpl {
 			   {'6', 'A', '9', 'C', '4', '5', '7', '1', '8', '3', 'G', '2', 'B', 'D', 'F', 'E'},
 			   {'F', '1', 'E', '4', '3', 'B', '2', 'C', '5', '9', '6', 'D', 'A', '7', '8', 'G'},
 			   {'G', '7', '3', 'D', 'A', 'F', 'E', '8', 'B', '4', '1', 'C', '6', '9', '2', '5'}};
-	   /** grille de test 9x9 */
+	   /** 
+	    * grille de test 9x9.
+	    * */
 	   private GrilleImpl Grille9x9Test = new GrilleImpl(Grille9x9Aremplir);
 	   
 	   /** test de la méthode getDimension()*/
@@ -104,7 +119,7 @@ public class TestGrilleImpl {
 	    @Test
 	    public void testGetValue2()
 	    {
-	       Assertions.assertThrows(IllegalArgumentException.class, () -> Grille9x9Test.getValue(15,1));
+	       Assertions.assertThrows(IllegalArgumentException.class, () -> Grille9x9Test.getValue(15, 1));
 	    }
 	    
 	    /**
@@ -141,14 +156,14 @@ public class TestGrilleImpl {
 	     */
 	    @Test
 	    public void TestPossible4() {
-	       Assertions.assertThrows(IllegalArgumentException.class, () -> Grille9x9Test.possible(0, 0,'z'));
+	       Assertions.assertThrows(IllegalArgumentException.class, () -> Grille9x9Test.possible(0, 0, 'z'));
 	    }
 	    /**
 	     * methode qui teste Possible() pour voir si c'est hors grille.
 	     */
 	    @Test
 	    public void TestPossible5() {
-	      Assertions.assertThrows(IllegalArgumentException.class, () -> Grille9x9Test.possible(34, 88,'a'));
+	      Assertions.assertThrows(IllegalArgumentException.class, () -> Grille9x9Test.possible(34, 88, 'a'));
 	    }
 
 }
